a7c983b134784164b2f1c48080126dbb431db104
OLIVER <U19303250@utp.edu.pe>
2021-09-24 18:05:14 -0500

    doble prueba

--- src/codigoproyecto/Codigoproyecto.java
+++ src/codigoproyecto/Codigoproyecto.java
@@ -15,7 +15,7 @@ public class Codigoproyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba");
+        System.out.println("prueba prueba");
     }
     
 }
